05bb8b1a6e27d295de22805a422ff4ef5bc013a9
H-kernel <hexinboy_sw@163.com>
2021-03-22 15:46:59 +0800

    fix

--- mov/include/mov-file-buffer.h
+++ mov/include/mov-file-buffer.h
@@ -2,11 +2,6 @@
 #define _mov_file_buffer_h_
 #include "mov-buffer.h"
 #include <stdio.h>
-#if defined(__cplusplus)
-extern "C" {
-#endif
+
 const struct mov_buffer_t* mov_file_buffer(void);
-#if defined(__cplusplus)
-}
-#endif
 #endif
